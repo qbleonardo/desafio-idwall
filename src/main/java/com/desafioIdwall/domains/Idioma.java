eadd6963069321efe79aeda8fde88c2a5e4509bc
qbleonardo <qbleonardo@gmail.com>
2023-11-24 11:53:13 -0300

    feat: implementacao do controller busca interpol / domain interpol

---
+++ src/main/java/com/desafioIdwall/domains/Idioma.java
@@ -0,0 +1,18 @@
+package com.desafioIdwall.domains;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Idioma {
+
+    private String idioma;
+
+}
---
+++ src/main/java/com/desafioIdwall/domains/ImagemInterpol.java
@@ -0,0 +1,17 @@
+package com.desafioIdwall.domains;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImagemInterpol {
+
+    private String imagem;
+}
